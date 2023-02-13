35f8fe10a2505624d3cfc9f65adf8c3d692d633f
ali.atguedda <ali.atguedda@gmail.com>
2023-02-13 01:36:05 +0100

    add xpath retrieval function

---
+++ index.js
@@ -0,0 +1,21 @@
+
+/**
+ * @param  {} domElement - DOM element for xpath generation
+ * @param  {} absolute=false - specify that the returned xpath should be in its absolute format
+ * @returns { string } - xpath value
+ */
+export const retrieveXPath = (domElement, absolute=false) => {
+    if(!absolute && domElement.id.length) {
+        return `//*[@id='${domElement.id}']`;
+    }
+    if (!absolute && document.getElementsByTagName(domElement.tagName).length === 1) {
+        return `//${domElement.tagName.toLowerCase()}`;
+    }
+    if(absolute && domElement.tagName.toLowerCase() === 'html') {
+        return '/html'
+    }
+    const nodesInSameLevel = Array.from(domElement.parentNode.childNodes);
+    const nodesInSameLevelWithSameTagName = nodesInSameLevel.filter(element => element.tagName === domElement.tagName);
+    const domElementLevelPosition = nodesInSameLevelWithSameTagName.indexOf(domElement);
+    return `${retrieveXPath(domElement.parentNode, absolute)}/${domElement.tagName.toLowerCase()}${`[${domElementLevelPosition + 1}]`}`;
+}
